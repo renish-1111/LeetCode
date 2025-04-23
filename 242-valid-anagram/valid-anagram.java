45c648bf21998125946f86bc368ff50402303fb7
Renish Ponkiya <134797412+renish-1111@users.noreply.github.com>
2025-04-23 22:04:45 +0530

    Time: 6 ms (43.71%) | Memory: 42.8 MB (94.92%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()){
+            return false;
+        }
+        int alph[] = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            alph[s.charAt(i) - 'a']++;
+            alph[t.charAt(i) - 'a']--;
+        }
+        for(int count:alph){
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
